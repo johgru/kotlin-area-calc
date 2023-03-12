576873b442d95d32da601c5bf88b5cdbc7794911
Johan <johgru95@gmail.com>
2023-03-12 09:54:23 +0100

    Update Main.kt

--- Main.kt
+++ Main.kt
@@ -3,10 +3,16 @@ import kotlin.math.sqrt
 const val PI: Double = 3.14
 
 fun main() {
+    
+    println("Options: triangle, circle, rectangle")
+
     when (readln()) {
         "triangle" -> {
+            println("Enter first side of triangle:")
             val a = readln().toDouble()
+            println("Enter second side of triangle:")
             val b = readln().toDouble()
+            println("Enter third side of triangle:")
             val c = readln().toDouble()
 
             val s = (a + b + c) / 2
@@ -14,14 +20,22 @@ fun main() {
         }
 
         "rectangle" -> {
+            println("Enter first side of rectangle:")
             val a = readln().toDouble()
+            println("Enter second side of rectangle:")
             val b = readln().toDouble()
             println(a * b)
         }
 
         "circle" -> {
+            println("Enter circle radius")
             val r = readln().toDouble()
             println((PI * r * r))
+        }
+
+        else -> {
+            println("Try again!")
+            main()
         }
     }
 }
