d93c36ab3f2353b0015b7e17e9be9761ac62f3a9
Johan <johgru95@gmail.com>
2023-03-12 09:50:50 +0100

    Create Main.kt

---
+++ Main.kt
@@ -0,0 +1,27 @@
+import kotlin.math.sqrt
+
+const val PI: Double = 3.14
+
+fun main() {
+    when (readln()) {
+        "triangle" -> {
+            val a = readln().toDouble()
+            val b = readln().toDouble()
+            val c = readln().toDouble()
+
+            val s = (a + b + c) / 2
+            println(sqrt(s * (s - a) * (s - b) * (s - c)))
+        }
+
+        "rectangle" -> {
+            val a = readln().toDouble()
+            val b = readln().toDouble()
+            println(a * b)
+        }
+
+        "circle" -> {
+            val r = readln().toDouble()
+            println((PI * r * r))
+        }
+    }
+}
